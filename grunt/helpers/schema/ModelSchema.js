b39867887eea953030cdb3e7fd460218520798db
lokesh <lokesh.krishnappa@laerdal.com>
2023-09-01 09:50:00 +0200

    removed the extra spaces

--- grunt/helpers/schema/ModelSchema.js
+++ grunt/helpers/schema/ModelSchema.js
@@ -10,7 +10,7 @@ class ModelSchema extends GlobalsSchema {
    * @returns {[string]}
    */
   getTranslatablePaths() {
-    const paths = {};  
+    const paths = {};
     this.traverse('', ({ description, next }, attributePath) => {
       switch (description.type) {
         case 'object':
@@ -45,7 +45,7 @@ class ModelSchema extends GlobalsSchema {
     }, '/');
   
     return Object.keys(paths);
-  }  
+  }
 
 }
 
